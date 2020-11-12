197feaed4f1b69fda00dbe0099c19c03a1cf49f4
Jessalves11 <57305546+Jessalves11@users.noreply.github.com>
2020-11-12 20:48:10 -0300

    Add files via upload

---
+++ dadosProj_Drone.m
@@ -0,0 +1,24 @@
+% ===== Trabalho 1 - ENGK38 =====
+clear all;clc;
+
+% ==> Parametros
+Cx = 0.3;
+Cy = 0.1;
+g = 9.8;
+Kz = 1.3;
+tz = 0.4;
+zmax = 1.0;
+wfi = 4.47;
+fimax = 0.26;
+zetafi = 0.5;
+wtheta = 4.47;
+thetamax = 0.26;
+zetatheta = 0.5;
+Kpsi = 1;
+tpsi = 0.3;
+psimax = 1.74;
+
+
+
+
+
